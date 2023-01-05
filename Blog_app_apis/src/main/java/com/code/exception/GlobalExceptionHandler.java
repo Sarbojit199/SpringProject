c62b9c65ac549fb28951067a7b71d0d95b675d00
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2023-01-05 21:19:37 +0530

    Update GlobalExceptionHandler.java

--- Blog_app_apis/src/main/java/com/code/exception/GlobalExceptionHandler.java
+++ Blog_app_apis/src/main/java/com/code/exception/GlobalExceptionHandler.java
@@ -4,5 +4,23 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+  
+  @ExceptionHandler(ResourseNotFoundException.class)
+	public ResponseEntity<ApiResponse> resourseNotFoundExceptionHandler(ResourseNotFoundException ex){
+		String message=ex.getMessage();
+		ApiResponse apiResponse =new ApiResponse(message,false);
+		return new ResponseEntity<ApiResponse> (apiResponse,HttpStatus.NOT_FOUND);
+	}
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String,String>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex){
+		Map<String,String> resp =new HashMap<>();
+		ex.getBindingResult().getFieldErrors().forEach(error -> {
+			String fieldName=((FieldError) error).getField();
+			String message=error.getDefaultMessage();
+			resp.put(fieldName,message);
+		});
+		return new ResponseEntity<Map<String,String>>(resp,HttpStatus.BAD_REQUEST);
+		
+	}
 
 }
